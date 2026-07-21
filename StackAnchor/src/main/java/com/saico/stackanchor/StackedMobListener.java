d812ce232e68c3caec1c7d37f423e0dee1391485
mjhtekkers <ohtekkersbusiness@gmail.com>
2026-07-21 17:08:07 +0100

    Update StackedMobListener.java

--- StackAnchor/src/main/java/com/saico/stackanchor/StackedMobListener.java
+++ StackAnchor/src/main/java/com/saico/stackanchor/StackedMobListener.java
@@ -3,10 +3,6 @@ package com.saico.stackanchor;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.Server;
-import org.bukkit.command.CommandSender;
-import org.bukkit.conversations.Conversation;
-import org.bukkit.conversations.ConversationAbandonedEvent;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -18,14 +14,9 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.metadata.FixedMetadataValue;
-import org.bukkit.permissions.Permission;
-import org.bukkit.permissions.PermissionAttachment;
-import org.bukkit.permissions.PermissionAttachmentInfo;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 
 import java.lang.reflect.Method;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -167,40 +158,10 @@ public class StackedMobListener implements Listener {
         int y = loc.getBlockY();
         int z = loc.getBlockZ();
 
-        String commandToRun = null;
         if (typeName.equals("SHEEP")) {
-            commandToRun = "serveritem drop sheephead 1 " + worldName + " " + x + " " + y + " " + z;
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "serveritem drop sheephead 1 " + worldName + " " + x + " " + y + " " + z);
         } else if (typeName.equals("PIG")) {
-            commandToRun = "serveritem drop pighead 1 " + worldName + " " + x + " " + y + " " + z;
-        }
-
-        if (commandToRun != null) {
-            CommandSender silentSender = new CommandSender() {
-                @Override public void sendMessage(String message) {}
-                @Override public void sendMessage(String[] messages) {}
-                @Override public Server getServer() { return Bukkit.getServer(); }
-                @Override public String getName() { return "StackAnchorSilent"; }
-                @Override public boolean isOp() { return true; }
-                @Override public void setOp(boolean value) {}
-                @Override public void sendRawMessage(String message) {}
-                @Override public boolean isPermissionSet(String name) { return true; }
-                @Override public boolean isPermissionSet(Permission perm) { return true; }
-                @Override public boolean hasPermission(String name) { return true; }
-                @Override public boolean hasPermission(Permission perm) { return true; }
-                @Override public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value) { return null; }
-                @Override public PermissionAttachment addAttachment(Plugin plugin) { return null; }
-                @Override public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value, int ticks) { return null; }
-                @Override public PermissionAttachment addAttachment(Plugin plugin, int ticks) { return null; }
-                @Override public void removeAttachment(PermissionAttachment attachment) {}
-                @Override public void recalculatePermissions() {}
-                @Override public Set<PermissionAttachmentInfo> getEffectivePermissions() { return Collections.emptySet(); }
-                @Override public boolean isConversing() { return false; }
-                @Override public void acceptConversationInput(String input) {}
-                @Override public boolean beginConversation(Conversation conversation) { return false; }
-                @Override public void abandonConversation(Conversation conversation) {}
-                @Override public void abandonConversation(Conversation conversation, ConversationAbandonedEvent details) {}
-            };
-            Bukkit.dispatchCommand(silentSender, commandToRun);
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "serveritem drop pighead 1 " + worldName + " " + x + " " + y + " " + z);
         }
     }
 
